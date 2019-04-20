9a6cb5406f778dba8fbd7e06b7cd06603606e792
herorest <herorest@163.com>
2019-04-20 14:54:18 +0800

    添加单例模式的GameEventDispatcher

--- assets/Script/GameEventDispatcher.ts
+++ assets/Script/GameEventDispatcher.ts
@@ -3,6 +3,15 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class GameEventDispatcher extends cc.Component {
 
+    private static _instance: GameEventDispatcher = null;
+
+    getInstance(): GameEventDispatcher{
+        if(GameEventDispatcher._instance == null){
+            GameEventDispatcher._instance = new GameEventDispatcher();
+        }
+        return GameEventDispatcher._instance;
+    }
+
     onLoad(){
 
     }
